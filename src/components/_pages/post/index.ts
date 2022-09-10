7979a49ee6a2abf0de0a093660aa0f7121efea8d
danpacho2 <110222738+danpacho2@users.noreply.github.com>
2022-09-10 13:41:30 +0900

    Initial commit

---
+++ src/components/_pages/post/index.ts
@@ -0,0 +1,4 @@
+export { default as PostNav } from "./PostNav"
+export { default as PostBanner } from "./PostBanner"
+export { default as PostMeta } from "./PostMeta"
+export { default as PostToc } from "./PostToc"
